eea1cddf80e915b825bbf1de45249f80f04d698a
artFch <artemfilichev02@yandex.ru>
2020-09-28 21:51:16 +0700

    Formated

--- src/functions.h
+++ src/functions.h
@@ -2,7 +2,7 @@
 #define FUNCTIONS_H
 
 void generate();
-int convertNumber(int number, int *guess);
-int bulls_cows(int *numk, int *guess);
-int cows(int *numk, int *guess,int bulls);
+int convertNumber(int number, int* guess);
+int bulls_cows(int* numk, int* guess);
+int cows(int* numk, int* guess, int bulls);
 #endif
